0a536d3e77b1e0aa3405576914a723f308147950
S.V.D.Tharindi <144718411+svdtharindi@users.noreply.github.com>
2024-01-10 21:35:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import React,{ useState } from 'react';
+
+
+const App = () => {
+  const[temperatureValue ,setTemperatureValue] = useState(10);
+  const[temperatureColor ,setTemperatureColor] = useState('cold');
+
+  
+
+  const increaseTemperature =() => {
+    if(temperatureValue ===30) return;
+    const newTemperature =temperatureValue +1;
+
+    if(newTemperature >= 15){
+      setTemperatureColor('hot');
+    }
+
+    setTemperatureValue(newTemperature);
+
+  };
+
+  const decreaseTemperature =() => {
+    if(temperatureValue ===-5) return;
+    const newTemperature =temperatureValue - 1;
+
+    if(newTemperature < 15){
+      setTemperatureColor('cold');
+    }
+
+    setTemperatureValue(newTemperature);
+
+  };
+
+  return(
+    <div className='app-container'>
+      <div className='temperature-display-container'>
+            <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C
+            </div>
+      </div>
+
+      <div className='button-container'>
+        <button onClick={()=> increaseTemperature()}>+</button>
+        <button onClick={()=> decreaseTemperature()}>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
